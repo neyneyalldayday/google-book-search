97f08a5b6f926363ed57b5f358370d0f41ac34cb
NeyNey <drfrankenstein123@gmail.com>
2020-12-14 13:54:44 -0600

    gotta go

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -4,10 +4,7 @@ const Home = () => (
   <div>
     <h1 className="title is-1">Books are Tiiight</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
-      risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
-      nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
-      lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.
+     nerd
     </p>
   </div>
 );
--- client/src/pages/search/index.js
+++ client/src/pages/search/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 const Search = () => (
-  <div>
-    <h1 className="title is-1">search the steeze</h1>
-    <p>
-      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-      inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
-      et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
-      in efficitur arcu congue. Nam fermentum commodo egestas.
-    </p>
+  <div class="container is-max-desktop">
+  <div class="notification is-primary">
+  <h1 className="title is-1">search for steeze</h1>
+<p>
+  Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+  inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
+  et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
+  in efficitur arcu congue. Nam fermentum commodo egestas.
+</p> 
   </div>
+</div>
+
 );
 
 export default Search;
