927ba37d468ce12c78d89cef285230201bcf338f
NeyNey <drfrankenstein123@gmail.com>
2020-12-14 13:52:43 -0600

    added a container

--- client/src/pages/saved/index.js
+++ client/src/pages/saved/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 
+
 const Saved = () => (
-  <div>
-    <h1 className="title is-1">your saved steeze</h1>
-    <p>
-      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-      inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
-      et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
-      in efficitur arcu congue. Nam fermentum commodo egestas.
-    </p>
+  
+  <div class="container is-max-desktop">
+  <div class="notification is-primary">
+  <h1 className="title is-1">your saved steeze</h1>
+<p>
+  Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+  inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
+  et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
+  in efficitur arcu congue. Nam fermentum commodo egestas.
+</p> 
   </div>
+</div>
+
+ 
+
+
+
+
+   
+  
 );
 
 export default Saved;
