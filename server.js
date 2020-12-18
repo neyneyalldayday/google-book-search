859bea63d01ae1317dddd97182b5b9eeeecd038f
NeyNey <drfrankenstein123@gmail.com>
2020-12-17 18:36:46 -0600

    went a long way to get here

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes =require("./routes")
+const mongoose =require("mongoose");
 
+mongoose.connect("mongodb://localhost/reactreadinglist");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,7 +13,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+app.use(routes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
