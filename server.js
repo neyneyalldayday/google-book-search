414c906f67786b725501bd022cf384a2ef16a1d1
NeyNey <drfrankenstein123@gmail.com>
2020-12-19 19:45:39 -0600

    make it better

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(routes);
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true} );
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist";
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true} );
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
