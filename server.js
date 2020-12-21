ac63ea29ad43361bf4e28ed043200636560e259a
NeyNey <drfrankenstein123@gmail.com>
2020-12-21 10:30:42 -0600

    deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
@@ -14,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(__dirname,"client/build"));
 }
 app.use(routes);
 
